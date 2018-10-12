97ce468751d2b5c9825ebb200e1c8e0bae18e2ab
Dragneel7 <sainisurya1@gmail.com>
2018-10-12 05:36:13 +0530

    add css

--- blog.php
+++ blog.php
@@ -1,8 +1,16 @@
-<html> 
+<html>
+
 <head>
-<link rel="stylesheet" href="blog.css">
+    <title>View Blogs</title>
+    <link rel="stylesheet" href="blog.css">
 </head>
+
 <body>
+
+<nav class="navbar navbar-expand-sm">
+        <a class="navbar-brand" href="https://img.channeli.in"> <h3>Workshop '18</h3> </a>
+        <a href="add_blog.html"><h3>Create</h3></a>
+</nav>
 
 <?php
 
@@ -12,12 +20,24 @@ $result = $conn->query($sql);
 
 if($result->num_rows>0){
     while($row = $result->fetch_assoc()){
-        echo '<h1>'.$row['post_title'].'</h1>';
+        echo '
+        <div class="container col-sm-8">    
+            <div class="card blog-card">
+                <div class="card-body">
+                    <div class="blog-header">
+                        <h3>'.$row['post_title'].'</h3>
+                    </div>
+                    <div class="blog-info">
+                        <p class="posted-on"> <i> Posted by '.$row['author'].' on '.$row['posted_on'].'</i> </p>
+                        <p class="card-text">'.$row['content'].'</p>
+                    </div>
+                </div>
+            </div>
+        </div>';
     }
 }
 
 ?>
 
-<a href='add_blog.html'> add blog</a>
 </body>
 </html>
